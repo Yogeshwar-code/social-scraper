5b74adc778a1b354255cafb2f72c16640029b581
Yogeshwar <yogeshwar@example.com>
2026-04-21 17:00:58 +0530

    auto create superuser on render

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
+
+python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model;
+User = get_user_model();
+User.objects.filter(username='Yogesh').exists() or
+User.objects.create_superuser('Yogesh', 'admin@gmail.com', 'yogesh@123')" \
+| python manage.py shell
+
 gunicorn backend.wsgi:application
